e52a0a24e329d7fbc17a5acaade195489bf9461e
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-08-02 12:09:08 +0500

    Improve the routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 import connectDB from "./database/mongo";
 import cors from "cors";
-import dotenv from "dotenv";
 import { todoRouter } from "./todo/todo.router";
 
 dotenv.config();
@@ -10,13 +10,13 @@ connectDB();
 const port: number = parseInt(process.env.PORT as string);
 const app: express.Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send(`GEt Api is working`);
-});
-
 app.use(cors());
 app.use(express.json());
-app.use("/api/list/todo", todoRouter);
+app.use("/api/todos", todoRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(`Get Api is working`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
