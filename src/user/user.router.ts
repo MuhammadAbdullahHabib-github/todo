ac046d5e3df4be1f96665e5d7125296cdb4617b0
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-08-14 18:00:01 +0500

    User Schema is added

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,9 +1,36 @@
 import express , {NextFunction, Request, Response} from "express";
+import * as UserService from "./user.service"
 export const userRouter = express.Router();
 import {validationResult,param,check} from 'express-validator';
+import { IUser } from "./user.interface";
 
-userRouter.post('/',(req:Request,res:Response) => {
-    
+const validateUserFields = [
+    check('firstname').not().isEmpty().withMessage('Please enter firstname'),
+    check('lastname').not().isEmpty().withMessage('Please enter lastname'),
+    check('email').not().isEmpty().withMessage('Please enter email'),
+    check('password').not().isEmpty().withMessage('Please enter password'),
+]
+
+const handleValdiaionResult = (req:Request,res:Response,next:NextFunction) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({error:error.array()});
+    }
+    next();
+};
+
+const handleServerError = (res:Response,error:any) => {
+    res.status(500).send(error.message);
+};
+
+userRouter.post('/', [...validateUserFields, handleValdiaionResult ], async (req:Request,res:Response) => {
+    try{
+       const newUser: IUser = req.body;
+       await UserService.createUser(newUser);
+       res.status(201).json(newUser);
+    }catch(error:any){
+        handleServerError(res,error)
+    }
 })
 
 
