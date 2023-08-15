cd32eabb374d9168ec7f573e0b26ba75f6f42b26
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-08-15 15:14:51 +0500

    Added Volume in docker compose

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,71 +1,59 @@
-import express , {NextFunction, Request, Response} from "express";
-import * as UserService from "./user.service"
+import express, { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import * as UserService from "./user.service";
 export const userRouter = express.Router();
-import {validationResult,param,check} from 'express-validator';
+import { validationResult, param, check } from "express-validator";
 import { IUser } from "./user.interface";
 
+const saltRounds = 10;
+const hashPasswordAsync = async (password: string): Promise<string> => {
+  const hashPassword = await bcrypt.hash(password, saltRounds);
+  return hashPassword;
+};
+
 const validateUserFields = [
-    check('firstname').not().isEmpty().withMessage('Please enter firstname'),
-    check('lastname').not().isEmpty().withMessage('Please enter lastname'),
-    check('email').not().isEmpty().withMessage('Please enter email'),
-    check('password').not().isEmpty().withMessage('Please enter password'),
-]
+  check("firstname").not().isEmpty().withMessage("Please enter firstname"),
+  check("lastname").not().isEmpty().withMessage("Please enter lastname"),
+  check("email").not().isEmpty().withMessage("Please enter email"),
+  check("password").not().isEmpty().withMessage("Please enter password"),
+];
 
-const handleValdiaionResult = (req:Request,res:Response,next:NextFunction) => {
-    const error = validationResult(req);
-    if(!error.isEmpty()){
-        return res.status(422).json({error:error.array()});
-    }
-    next();
+const handleValdiaionResult = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(422)
+      .json({ error: "Validation Failed", details: error.array() });
+  }
+  next();
 };
 
-const handleServerError = (res:Response,error:any) => {
-    res.status(500).send(error.message);
+const handleServerError = (res: Response, error: any) => {
+  console.log(error.message);
+  res.status(500).send(error.message);
 };
 
-userRouter.post('/', [...validateUserFields, handleValdiaionResult ], async (req:Request,res:Response) => {
-    try{
-       const newUser: IUser = req.body;
-       await UserService.createUser(newUser);
-       res.status(201).json(newUser);
-    }catch(error:any){
-        handleServerError(res,error)
+userRouter.post(
+  "/",
+  [...validateUserFields, handleValdiaionResult],
+  async (req: Request, res: Response) => {
+    try {
+      const { firstname, lastname, email, password }: IUser = req.body;
+      const hashedPassword = await hashPasswordAsync(password);
+      const newUser: Partial<IUser> = {
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+      };
+      const user = await UserService.createUser(newUser);
+      res.status(201).json(user);
+    } catch (error: any) {
+      handleServerError(res, error);
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+);
