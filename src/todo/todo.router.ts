03fa110e60f785ef0d2f5bb097241bc99b9c057e
Muhammad Abdullah Habib <abdullah.mohammad2019274@gmail.com>
2023-08-16 13:33:15 +0500

    feat: Add user controller

--- src/todo/todo.router.ts
+++ src/todo/todo.router.ts
@@ -1,41 +1,22 @@
-import { IAuthMiddlewareRequest } from "./../auth/auth.interface";
-import express, { NextFunction, Request, Response } from "express";
+import express, { Response } from "express";
 import { middleware } from "./../auth/auth.middleware";
-import * as TodoService from "./todo.service";
-import { param, check, validationResult } from "express-validator";
-import { ITodo } from "./todo.interface";
+
+import {
+  validateMongoIdParam,
+  validateTodoFields,
+  handleValidationResult,
+} from "./todo.controller";
+
+import {
+  createController,
+  findController,
+  findAllController,
+  updateController,
+  patchController,
+  deleteController,
+} from "./todo.controller";
 
 export const todoRouter = express.Router();
-
-const validateMongoIdParam = param("id")
-  .isMongoId()
-  .withMessage("ID should be a valid mongoID");
-
-const validateTodoFields = [
-  check("title").not().isEmpty().withMessage("Please enter title"),
-  check("description").not().isEmpty().withMessage("Please enter description"),
-];
-
-const handleValidationResult = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(422).json({ error: error.array() });
-  }
-  next();
-};
-
-const handleNotFoundError = (res: Response) => {
-  return res.status(404).send("Resource not found");
-};
-
-const handleServerError = (res: Response, error: any) => {
-  console.error(error);
-  res.status(500).send("Internal server error");
-};
 
 // @route POST /api/todo
 // @desc Create a todo
@@ -44,16 +25,7 @@ const handleServerError = (res: Response, error: any) => {
 todoRouter.post(
   "/",
   [...validateTodoFields, handleValidationResult, middleware],
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const todo: ITodo = req.body;
-      todo.user_id = req.user;
-      const newTodo = await TodoService.createTodo(todo);
-      res.status(201).json(newTodo);
-    } catch (error) {
-      handleServerError(res, error);
-    }
-  }
+  createController
 );
 
 // @route GET /api/todo/:id
@@ -63,41 +35,14 @@ todoRouter.post(
 todoRouter.get(
   "/:id",
   [validateMongoIdParam, handleValidationResult, middleware],
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const user_id = req.user;
-      const todo: ITodo | null = await TodoService.find(id, user_id);
-      if (!todo) {
-        handleNotFoundError(res);
-      }
-      return res.status(200).json(todo);
-    } catch (error: any) {
-      handleServerError(res, error);
-    }
-  }
+  findController
 );
 
 // @route GET /api/todo
 // @desc Get all todos
 // @access Private
 
-todoRouter.get(
-  "/",
-  middleware,
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const user_id = req.user;
-      const allTodos: ITodo[] | null = await TodoService.findAll(user_id);
-      if (!allTodos) {
-        handleNotFoundError(res);
-      }
-      return res.status(200).json(allTodos);
-    } catch (error: any) {
-      handleServerError(res, error);
-    }
-  }
-);
+todoRouter.get("/", middleware, findAllController);
 
 // @route PUT /api/todo/:id
 // @desc Update a todo
@@ -111,20 +56,7 @@ todoRouter.put(
     handleValidationResult,
     middleware,
   ],
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const updatedTodo: ITodo = req.body;
-      const user_id = req.user;
-      const updatedItem = await TodoService.update(id, updatedTodo, user_id);
-      if (!updatedItem) {
-        handleNotFoundError(res);
-      }
-      res.status(200).json(updatedItem);
-    } catch (error: unknown) {
-      handleServerError(res, error);
-    }
-  }
+  updateController
 );
 
 // @route PATCH /api/todo/:id
@@ -133,20 +65,7 @@ todoRouter.put(
 todoRouter.patch(
   "/:id",
   [validateMongoIdParam, handleValidationResult, middleware],
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const updatedTodo: ITodo = req.body;
-      const user_id = req.user;
-      const updatedItem = await TodoService.patch(id, updatedTodo, user_id);
-      if (!updatedItem) {
-        handleNotFoundError(res);
-      }
-      res.status(204).json(updatedItem);
-    } catch (error: unknown) {
-      handleServerError(res, error);
-    }
-  }
+  patchController
 );
 
 // @route DELETE /api/todo/:id
@@ -156,17 +75,5 @@ todoRouter.patch(
 todoRouter.delete(
   "/:id",
   [validateMongoIdParam, handleValidationResult, middleware],
-  async (req: IAuthMiddlewareRequest, res: Response) => {
-    try {
-      const id: string = req.params.id;
-      const user_id = req.user;
-      const deleteItem = await TodoService.remove(id,user_id);
-      if (deleteItem === null) {
-        handleNotFoundError(res);
-      }
-      res.status(204);
-    } catch (error) {
-      handleServerError(res, error);
-    }
-  }
+  deleteController
 );
